f5e1244a5cb214458688988f04c60fabe151c6a4
Vivek K Shrivastava <99794442+mrvivekk@users.noreply.github.com>
2023-05-26 23:21:08 +0530

    Update Simple.java
    Added new update in source code

--- Simple.java
+++ Simple.java
@@ -3,5 +3,6 @@
 class Simple{
    public static void main(String[] args) {
       System.out.println("Welcome to Git's online Training!!!");
+      System.out.println("Git's online Training by Bhasker Reddy!!!");
    }
 }
